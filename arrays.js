807c0f11dc3306f1748bd89892dbc81d9a3f4567
Monica Quintana-Stiles <mca_quintana@yahoo.com>
2017-12-29 20:06:11 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -28,8 +28,8 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 }
 
 function removeElementFromBeginningOfArray(array){
-  array.slice(1)
-  return array
+  return array.slice(1)
+
 }
 
 function destructivelyRemoveElementFromEndOfArray(array){
@@ -38,6 +38,6 @@ function destructivelyRemoveElementFromEndOfArray(array){
 }
 
 function removeElementFromEndOfArray(array){
-  array.slice(0, array.length - 1)
-  return array
+  return array.slice(0, array.length - 1)
+
 }
